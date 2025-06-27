ecf2957113010df49b69264952d180c4eaf2b9cd
LeticiaParreiras <ticiaxp@gmail.com>
2025-06-26 22:59:04 -0300

    add route for favarito

--- frontend/src/pages/favoritos.tsx
+++ frontend/src/pages/favoritos.tsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../services/api';
-import { logout } from '../services/auth';
 import '../assets/styles/home.css'
 import type { Loja } from '../assets/interface/Loja';
 
-export default function HomeCliente() {
+export default function Favoritos() {
   const [lojas, setLojas] = useState<Loja[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchLojas() {
       try {
-        const res = await api.get('/loja');
+        const res = await api.get('/favoritos');
         setLojas(res.data);
       } catch {
         alert('Erro ao carregar lojas');
@@ -29,16 +28,13 @@ export default function HomeCliente() {
 
   return (
     <>
-    <header className="home-header">
+      <header className="home-header">
         <h1>Frutelo</h1>
         <button className="button" onClick={handleLogout}>Sair</button>
       </header>
-      
       <div className="home-container">
-        <div className="title">
-      <h2>Lojas</h2>
-      <button onClick={() => navigate(`/favoritos`)}><img src="/heart.svg" alt="coração" />Favoritos</button>
-        </div>
+        <p onClick={()=> navigate('/homeclient')} className='back'> &lt;</p>
+        <h2>Lojas Favoritas</h2>
       <div className="loja-list">
         {lojas.length > 0 ? ( 
           lojas.map((loja: Loja) => (
